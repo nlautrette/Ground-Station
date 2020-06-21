163f413675975cdc177bb039595b787ac2dae825
Vainavi Viswanath <vainaviv2001@berkeley.edu>
2020-06-20 19:29:28 -0700

    cleaned up testing_new_limits_serial2.py

--- real_time_control/testing_new_limits_serial2.py
+++ real_time_control/testing_new_limits_serial2.py
@@ -14,17 +14,11 @@ ser.flushInput()
 plot_window = 1000
 display = True
 
-sensors = 4 #set to how many are connected 
+sensors = 4 #set to how many sensors are connected 
 all_sensor_data = []
 
 for i in range(sensors):
     all_sensor_data.append([]) #store y_first, y_second, etc. in 2D array
-
-'''y_first = []
-y_second = []
-y_third = []
-y_fourth = []
-y_fifth = []'''
 
 plt.ion()
 fig, ax = plt.subplots(2,3)
@@ -49,12 +43,6 @@ if sensors <=5:
 if sensors <= 6:
     plot6, = ax[1,2].plot(all_sensor_data[5])
     all_plots.append(plot6)
-
-'''plot1, = ax[0,0].plot(y_first)
-plot2, = ax[0,1].plot(y_second)
-plot3, = ax[0,2].plot(y_third)
-plot4, = ax[1,0].plot(y_fourth)
-plot5, = ax[1,1].plot(y_fifth)'''
 
 last_first_value = 0
 last_values = [0] * 5
@@ -82,33 +70,6 @@ while True:
             all_sensor_data[i] = np.append(all_sensor_data[i],values[i])
             all_plots[i].set_ydata(all_sensor_data[i])
             all_plots[i].set_xdata(range(len(all_sensor_data[i])))
-
-        '''y_first = np.append(y_first,values[0])
-        y_second = np.append(y_second,values[1])
-        y_third = np.append(y_third,values[2])
-        y_fourth = np.append(y_fourth, values[3])
-        y_fifth = np.append(y_fifth, values[4])
-        data = [y_first, y_second, y_third, y_fourth, y_fifth]
-
-#        if len(y_first) >= 900:
-#            for list in data:
-#                list = list[len(list) - 900:]
-
-
-        plot1.set_ydata(y_first)
-        plot1.set_xdata(range(len(y_first)))
-
-        plot2.set_ydata(y_second)
-        plot2.set_xdata(range(len(y_second)))
-
-        plot3.set_ydata(y_third)
-        plot3.set_xdata(range(len(y_third)))
-
-        plot4.set_ydata(y_fourth)
-        plot4.set_xdata(range(len(y_fourth)))
-
-        plot5.set_ydata(y_fifth)
-        plot5.set_xdata(range(len(y_fifth)))'''
 
         # x.set_xlim(0, len(y_var))
         c = sys.stdin.read(1)
