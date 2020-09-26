4de3654d3472fb3fc0e90d1496922642125e9d22
Vamshi Balanaga <vamshi.balanaga@gmail.com>
2020-09-25 21:30:56 -0700

    use solenoids library

--- DataMonitoring/solenoids_rf_pressure/solenoids_rf_pressure.ino
+++ DataMonitoring/solenoids_rf_pressure/solenoids_rf_pressure.ino
@@ -1,4 +1,7 @@
 #include <math.h>
+#include <solenoids.h>
+
+Solenoids valves;
 
 #define LOW_PRESSURE_1 A0
 #define LOW_PRESSURE_2 A1
@@ -8,27 +11,7 @@
 #define HIGH_PRESSURE_1 A4
 #define HIGH_PRESSURE_2 A5
 
-#define LOX_2 2
-#define LOX_5 3
-#define LOX_GEMS 4
-
-#define PROP_2 8
-#define PROP_5 9
-#define PROP_GEMS 10
-
 //#define RFSerial Serial1
-
-// start solenoid states
-
-int lox2_state = LOW;
-int lox5_state = LOW;
-int lox_gems_state = LOW;
-
-int prop2_state = LOW;
-int prop5_state = LOW;
-int prop_gems_state = LOW;
-
-// end solenoid states
 
 void readData();
 void convertData();
@@ -44,29 +27,15 @@ int numHighPressure = 0;
 
 void setup() {
   Serial.begin(9600);
+  
   //Setup and start RF communication
 //  RFSerial.begin(57600);
 
-  //while(!Serial.available());
-  
-  pinMode(LOX_2, OUTPUT);
-  pinMode(LOX_5, OUTPUT);
-  pinMode(LOX_GEMS, OUTPUT);
-
-  pinMode(PROP_2, OUTPUT);
-  pinMode(PROP_5, OUTPUT);
-  pinMode(PROP_GEMS, OUTPUT);
-
-  digitalWrite(LOX_2, lox2_state);
-  digitalWrite(LOX_5, lox5_state);
-  digitalWrite(LOX_GEMS, lox_gems_state);
-
-  digitalWrite(PROP_2, prop2_state);
-  digitalWrite(PROP_5, prop5_state);
-  digitalWrite(PROP_GEMS, prop_gems_state);
+  // Setup solenoids
+  valves.init();
   
   
-Serial.println("How many low pressure sensors are connected?");
+  Serial.println("How many low pressure sensors are connected?");
   while (Serial.available() == 0) {
     
       delay(50);
@@ -137,45 +106,28 @@ void loop() {
   currTime = millis();
   if((currTime%int(periodic)) == 0) {
     if (Serial.available() > 0) {
-       char readByte = Serial.read();
-      if(readByte == 'a'){
-        lox2_state = toggle(lox2_state);
-        digitalWrite(LOX_2, lox2_state);
+      int readByte = Serial.read();
+      if(readByte == 'a') {
         Serial.print("Toggled LOX 2: ");
-        Serial.println(lox2_state);
-      } else if(readByte == 'b'){
-        lox5_state = toggle(lox5_state);
-        digitalWrite(LOX_5, lox5_state);
+        Serial.println(valves.toggleLOX2Way());
+      } else if(readByte == 'b') {
         Serial.print("Toggled LOX 5: ");
-        Serial.println(lox5_state);
-      } else if (readByte == 'c'){
-        lox_gems_state = toggle(lox_gems_state);
-        digitalWrite(LOX_GEMS, lox_gems_state);
+        Serial.println(valves.toggleLOX5Way());
+      } else if (readByte == 'c') {
         Serial.print("Toggled LOX Gems: ");
-        Serial.println(lox_gems_state);
-      } else if(readByte == 'x'){
-        prop2_state = toggle(prop2_state);
-        digitalWrite(PROP_2, prop2_state);
+        Serial.println(valves.toggleLOXGems());
+      } else if(readByte == 'x') {
         Serial.print("Toggled PROP 2: ");
-        Serial.println(prop2_state);
+        Serial.println(valves.toggleProp2Way());
       } else if (readByte == 'y') {
-        prop5_state = toggle(prop5_state);
-        digitalWrite(PROP_5, prop5_state);
         Serial.print("Toggled PROP 5: ");
-        Serial.println(prop5_state);
-      } else if (readByte == 'z'){
-        prop_gems_state = toggle(prop_gems_state);
-        digitalWrite(PROP_GEMS, prop_gems_state);
+        Serial.println(valves.toggleProp5Way());
+      } else if (readByte == 'z') {
         Serial.print("Toggled PROP Gems: ");
-        Serial.println(prop_gems_state);
+        Serial.println(valves.togglePropGems());
       }
     }
 
-//    if (millis() - currTime2 >= 1000) {
-//       Serial.println("beans!"); 
-//       currTime2 = millis();
-//    }
-  
     readData();
   
     convertData();
@@ -297,13 +249,4 @@ float lowPressureConversion(int raw){
 
 float highPressureConversion(int raw){
   return ((((float)raw * .6949) / 1024) * 4.8 - 1.3) * (5000/3.5) +200;
-}
-
-int toggle(int input_state){
-  if(input_state == LOW){
-     input_state = HIGH;
-  } else if (input_state == HIGH){
-     input_state = LOW;
-  }
-  return input_state;
 }
